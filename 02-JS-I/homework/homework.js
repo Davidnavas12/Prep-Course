2ccadf68df5e8f66d818e29fe90d50d7e0db29b8
David Nava <davidnava@MacBook-Pro-de-David.local>
2022-10-02 18:06:08 -0500

    first push

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -37,12 +37,17 @@ function suma(x, y) {
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
   
+  var suma = x + y
+  return suma
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
   
+  var resta = x - y
+  return resta 
+
 }
 
 function multiplica(x, y) {
