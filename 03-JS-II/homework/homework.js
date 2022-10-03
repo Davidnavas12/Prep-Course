0bf58d8587fb2ceae2c0eac700faa9df593cd6be
David Nava <davidnava@MacBook-Pro-de-David.local>
2022-10-03 00:48:46 -0500

    first push

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -203,12 +203,20 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
+  if (valor = true)
+    return 'Soy verdadero'
+
+  else (valor = false)
+    return 'Soy falso'
+
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
+
+
   
 }
 
@@ -216,12 +224,18 @@ function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
   
+  if (numero = tieneTresDigitos )
+    return true
+
+  else  
+    return false
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+
 }
 
 
