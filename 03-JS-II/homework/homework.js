3bab65de948eb2940bff903132221fc805010155
David Nava <davidnava@MacBook-Pro-de-David.local>
2022-10-03 00:38:41 -0500

    first push

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -189,7 +189,14 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
+  (numero > 1)
+
+  if (numero % numero || numero % 1)
+    return true  
 }
+
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
