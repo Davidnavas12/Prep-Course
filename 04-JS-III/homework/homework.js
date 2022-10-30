1ad4331d3548ec9a2e24242d78d3982999008c11
David Nava <davidnava@MacBook-Pro-de-David.local>
2022-10-29 22:52:14 -0500

    p# Please enter the commit message for your changes. Lines starting git commit .

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -288,6 +288,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  
+  var newarray = []
+  var suma = numero
+
+    for (var i = 0 ; i < 10 ; i++) {
+      if (i === 5 ) continue
+      suma = suma + 2
+      newarray.push (suma)
+
+    }
+    return newarray
 }
 
 
